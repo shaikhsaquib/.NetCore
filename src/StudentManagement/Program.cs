c260fa482f901c3c0ae3b1e5530b2604dba5f63e
kaif shaikh <kaifsh1733@gmail.com>
2026-02-10 01:36:27 +0530

    commit changes

--- src/StudentManagement/Program.cs
+++ src/StudentManagement/Program.cs
@@ -4,11 +4,19 @@ using StudentManagement.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();   // comes from Swashbuckle
+
 builder.Services.AddSingleton<IStudentRepository, InMemoryStudentRepository>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 
 var app = builder.Build();
 
-app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.MapControllers();
 app.Run();
